487db0656fc85acdcfa66f88ac1f7c81f44efb14
erikgonzalez <erik.gonzalez.ac@gmail.com>
2020-09-30 21:26:54 -0700

    Metodo para subir imagenes.

--- controllers/article.js
+++ controllers/article.js
@@ -1,6 +1,9 @@
 'use strict'
 
 var validator = require('validator');
+var fs = require('fs');
+var path = require('path');
+
 var Article = require('../models/article');
 const { param } = require('../routes/article');
 
@@ -223,6 +226,71 @@ var controller = {
             
         });
        
+    },
+
+    upload: (req , res) =>{
+        
+        // Configurar el modulo connect multiparty router/article.js
+
+        // recoger el fichero de la peticion
+        var file_name = 'Imagen no subida';
+
+        if(!req.files){
+            return res.status(404).send({
+                status: 'error',
+                message: file_name
+            });
+        }
+       
+        //conseguir el nombre y la extrension del archivo
+        var file_path = req.files.file0.path;
+        var file_split = file_path.split('\\');
+
+        // nombre del archivo
+        var file_name = file_split[2];
+
+        // extension del fichero
+        var extension_split = file_name.split('\.');
+        var file_ext = extension_split[1];
+
+        //comprobar la extension, solo imagenes, si no es valida borrar
+
+        if( file_ext != 'png' && file_ext != 'jpg' && file_ext != 'jpeg' && file_ext != 'gif' ){
+            //borrar el archivo subido
+            fs.unlink(file_path, (err)=>{
+                return res.status(500).send({
+                    status: 'error',
+                    message: 'La extensión de la imagen no es valida.'
+                });
+            });
+        }else{
+            // si es valido
+            var articleId = req.params.id;
+            // buscar el articulo y asignarle el nombre del archivo y subirlo
+            Article.findOneAndUpdate({_id: articleId}, {image: file_name}, {new:true}, (err, articleUpdated)=>{
+
+                if(err || !articleUpdated){
+                    return res.status(404).send({
+                        status: 'error',
+                        message: 'Error al guardar la imagen del articulo.'
+                    });
+
+                }
+
+                return res.status(200).send({
+                    status: 'success',
+                    article: articleUpdated
+                });
+
+            });
+
+            
+        }
+
+        
+
+
+        
     }
 
 
